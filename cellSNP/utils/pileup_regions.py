15165d7c1779676b23ce8db0cbe3122c3092c4c5
hxj5 <huang_xianjie@126.com>
2020-08-01 12:17:15 +0800

    fix index issue between mode 2 and 3

--- cellSNP/utils/pileup_regions.py
+++ cellSNP/utils/pileup_regions.py
@@ -107,7 +107,7 @@ def pileup_regions(samFile, barcodes, out_file=None, chrom=None, cell_tag="CR",
             continue
 
         base_list, qual_list, UMIs_list, cell_list = pileup_bases(pileupcolumn, 
-            pileupcolumn.pos, cell_tag, UMI_tag, min_MAPQ, max_FLAG, min_LEN)
+            pileupcolumn.pos + 1, cell_tag, UMI_tag, min_MAPQ, max_FLAG, min_LEN)
         
         if len(base_list) < min_COUNT:
             continue
